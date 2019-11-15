7cba2d39a163f2061a05d3081965c650c31dad0c
shagova-yana <bellamy5@mail.ru>
2019-11-15 11:54:10 +0300

    урок 5

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -242,6 +242,8 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  */
 fun canBuildFrom(chars: List<Char>, word: String): Boolean =
     word.toSet() == chars.toSet().intersect(word.toSet()) || word == ""
+            || word.toUpperCase().toSet() == chars.toSet().intersect(word.toSet())
+            || word.toLowerCase().toSet() == chars.toSet().intersect(word.toSet())
 
 
 
